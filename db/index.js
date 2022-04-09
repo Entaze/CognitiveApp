74241409d3a19b66bc1b540897041ddf655023c1
David Unuigbe <mowems@gmail.com>
2022-04-09 05:28:03 +0200

    Implemented index file

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-const mongoPath = require('./dbconfig.js');
-mongoose.connect(mongoPath, {useNewUrlParser: true,
+//Lanch ec2 instance for Mongodb. Point mongoPath to Ec2 mongo schema
+// const mongoPath = require('./dbconfig.js');
+mongoose.connect('mongodb://admin:Thanmolef10305@18.212.159.116:27017/pupper', {useNewUrlParser: true,
   useUnifiedTopology: true});
 
 

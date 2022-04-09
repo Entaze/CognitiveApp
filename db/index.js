6d2bcc1d2a3c71ee80fb6e28c20f3b39dfae402e
David Unuigbe <mowems@gmail.com>
2022-04-09 06:43:43 +0200

    Index added

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //Lanch ec2 instance for Mongodb. Point mongoPath to Ec2 mongo schema
 // const mongoPath = require('./dbconfig.js');
-mongoose.connect('mongodb+srv://moadmin:chewychewy@cluster0.bnaep.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true,
+mongoose.connect('mongodb+srv://moadmin:chewychewy@cluster0.bnaep.mongodb.net/cogniapp?retryWrites=true&w=majority', {useNewUrlParser: true,
   useUnifiedTopology: true});
 
 

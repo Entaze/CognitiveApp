5f96b9cba5a620a0935f403217683613d6278a2c
David Unuigbe <mowems@gmail.com>
2022-05-10 17:02:45 +0200

    Implemented Lazy Loading in index.js Routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,25 @@
 import ReactDOM from 'react-dom';
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import WithNavi from './Components/Layout/WithNavigation.jsx';
 import WithoutNavi from './Components/Layout/WithoutNavigation.jsx';
 import MainProvider from './Components/Providers/MainProvider.jsx';
-import Login from './Components/Login/Login.jsx';
-import Profile from './Components/Profile/Profile.jsx';
-import Cognitivetest from './Components/cognitivetest/Cognitivetest.jsx';
-import Cognitivetest1s2 from './Components/cognitivetest/Cognitivetest1s2.jsx';
+// import Login from './Components/Login/Login.jsx';
+// import Profile from './Components/Profile/Profile.jsx';
+// import Cognitivetest from './Components/cognitivetest/Cognitivetest.jsx';
+// import Cognitivetest1s2 from './Components/cognitivetest/Cognitivetest1s2.jsx';
 
 const appElement = document.getElementById('app');
+const Login = lazy(() => import('./Components/Login/Login.jsx'));
+const Profile = lazy(() => import('./Components/Profile/Profile.jsx'));
+const Cognitivetest = lazy(() => import('./Components/cognitivetest/Cognitivetest.jsx'));
+const Cognitivetest1s2 = lazy(() => import('./Components/cognitivetest/Cognitivetest1s2.jsx'));
 
 ReactDOM.render(
   <MainProvider>
     <BrowserRouter>
+    <Suspense fallback={<div>Loading...</div>}>
       {/* <Typography style={{ fontSize: 30, fontWeight: 700, color: '#ff9800', textAlign: 'center', fontFamily:'Courgette' }}>Pupper</Typography> */}
       <Routes>
         <Route element={<WithoutNavi />}>
@@ -27,6 +32,8 @@ ReactDOM.render(
           <Route path="cognitivetest1s2" element={<Cognitivetest1s2 />} />
         </Route>
       </Routes>
+    </Suspense>
+
     </BrowserRouter>
   </MainProvider>,
   appElement

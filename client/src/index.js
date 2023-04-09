c04fc80c4dd400e260abb316b1338691a7d3dffc
David Unuigbe <mowems@gmail.com>
2023-04-09 14:15:10 +0200

    Recent Changes

--- client/src/index.js
+++ client/src/index.js
@@ -15,12 +15,12 @@ const ConfirmAccount = lazy(() => import('./Components/Login/ConfirmAccount.jsx'
 const ResetPassword = lazy(() => import('./Components/Login/ResetPassword.jsx'));
 const CognitiveTest1 = lazy(() => import('./Components/cognitivetest/Test1/CognitiveTest1.jsx'));
 const CognitiveTest2 = lazy(() => import('./Components/cognitivetest/Test2/CognitiveTest2.jsx'));
-// const CognitiveTest3 = lazy(() => import('./Components/cognitivetest/Test3/CognitiveTest3.jsx'));
-// const CognitiveTest4 = lazy(() => import('./Components/cognitivetest/Test4/CognitiveTest4.jsx'));
+const CognitiveTest3 = lazy(() => import('./Components/cognitivetest/Test3/CognitiveTest3.jsx'));
+const CognitiveTest4 = lazy(() => import('./Components/cognitivetest/Test4/CognitiveTest4.jsx'));
 const CognitiveTest1Recall = lazy(() => import('./Components/cognitivetest/Test5/CognitiveTest1Recall.jsx'));
 const CognitiveTest2Recall = lazy(() => import('./Components/cognitivetest/Test6/CognitiveTest2Recall.jsx'));
-// const CognitiveTest3Recall = lazy(() => import('./Components/cognitivetest/Test7/CognitiveTest3Recall.jsx'));
-// const CognitiveTest4Recall = lazy(() => import('./Components/cognitivetest/Test8/CognitiveTest4Recall.jsx'));
+const CognitiveTest3Recall = lazy(() => import('./Components/cognitivetest/Test7/CognitiveTest3Recall.jsx'));
+const CognitiveTest4Recall = lazy(() => import('./Components/cognitivetest/Test8/CognitiveTest4Recall.jsx'));
 
 const CountdownTimer = lazy(() => import('./Components/cognitivetest/countdownTimer/countdownTimer.jsx'));
 const CountdownTimer2 = lazy(() => import('./Components/cognitivetest/countdownTimer2/countdownTimer2.jsx'));
@@ -50,14 +50,14 @@ ReactDOM.render(
       <Route path="/countdown" element={<CountdownTimer />} />
       <Route path="/countdown2" element={<CountdownTimer2 />} />
 
-      {/* <Route path="/cognitivetest4" element={<CognitiveTest4 />} />
-      <Route path="/cognitivetest3" element={<CognitiveTest3 />} /> */}
+      <Route path="/cognitivetest4" element={<CognitiveTest4 />} />
+      <Route path="/cognitivetest3" element={<CognitiveTest3 />} />
       <Route path="/cognitivetest2" element={<CognitiveTest2 />} />
       <Route path="/cognitivetest1" element={<CognitiveTest1 />} />
       <Route path="/cognitivetest1recall" element={<CognitiveTest1Recall />} />
       <Route path="/cognitivetest2recall" element={<CognitiveTest2Recall />} />
-      {/* <Route path="/cognitivetest3recall" element={<CognitiveTest3Recall />} />
-      <Route path="/cognitivetest4recall" element={<CognitiveTest4Recall />} /> */}
+      <Route path="/cognitivetest3recall" element={<CognitiveTest3Recall />} />
+      <Route path="/cognitivetest4recall" element={<CognitiveTest4Recall />} />
 
       <Route path="/test-begin" element={<Welcome />} />
       <Route path="/test-end" element={<Finish />} />
